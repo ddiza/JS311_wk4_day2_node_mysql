b5e5659c8a47f91b642aa9a5887b1469ca0b321f
ddiza <dantediza@pm.me>
2023-08-15 01:25:29 -0500

    updated queries

--- controllers/users.js
+++ controllers/users.js
@@ -19,39 +19,73 @@ const getAllUsers = (req, res) => {
       return res.json(rows);
     }
   })
-}
-//.OLDER SYNTAX to the above code block:
+
+// OLDER SYNTAX replaced by above code block:
 //   pool.query("SELECT * FROM users", (err, rows) => {
 //     if (err) return handleSQLError(res, err)
 //     return res.json(rows);
 //   })
 // }
-
-const getUserById = (req, res) => {
-  // SELECT USERS WHERE ID = <REQ PARAMS ID>
-  let id = req.params.id
-  let params = [id]
-
-  let sql = "SELECT * FROM users u "
-  sql += "JOIN usersAddress ua ON u.id = ua.user_id "
-  sql += "JOIN usersContact uc ON u.id = uc.user_id "
-  sql += "AND u.id = ?" 
-
-
-
-  // WHAT GOES IN THE BRACKETS
-  db.query(sql, params, (err, rows) => {
-    if (err){
-      console.log("getUserById query failed", err)
-      res.sendStatus(400)
-    } else {
-      res.json(rows);
-    }
-  })
 }
 
 
-//OLDER SYNTAX:  
+const getUserById = (req, res) => {
+  // SELECT USERS WHERE ID = <REQ PARAMS ID>
+  // users/:id
+  // put the requested pat id into a variable
+
+  let id = req.params.id //This gets wtv is sent from front-end form in id text field.
+  let params = [id]; // You can have more than one ? in your query &  must be in order of use in the query.
+  
+  // CHECK FOR VALID ID
+  if(!id){
+    res.sendStatus(400); // user fault; didn't send valid id
+    return;              // and stop. no reason to execute further
+  }
+
+  // THE FOLLOWING IS IS A BAD WAY TO DO A QUERY: 
+  // with the id (or any request param) concatinated to the string.
+  // WHY? If I add and drop table users.
+  // This is called a SQL Injection Attack
+  // let sql = "SELECT first_name, last_name, address, city, county, state, zip, phone1, phone2, email "
+  // sql += "FROM users u "
+  // sql += "JOIN usersAddress ua on u.id = ua.user_id "
+  // sql += "JOIN usersContact uc on u.id = uc.user_id"
+  // sql += "where u.id = " + id;
+
+  // Instead, we use parameterized sql statements
+  let sql = "SELECT first_name, last_name, address, city, county, state, zip, phone1, phone2, email "
+  sql += "FROM users u "
+  sql += "JOIN usersAddress ua on u.id = ua.user_id "
+  sql += "JOIN usersContact uc on u.id = uc.user_id"
+  sql += "where u.id = ?"; 
+  // The ? is a dynamic value, restricted to query parameter.
+  // The query param isn't combined with the main query until after the query has been 
+  // parsed. So there's no way the parameter can introduce unintended syntax.
+
+  db.query(sql, params, (err,rows) => {
+    if (err){
+      console.log("getUserById query failed ", err)
+      res.sendStatus(500); // It was our fault; could be db.
+    } else {
+      // we got results, but we got more than one row
+      if(rows.length > 1){
+        console.log("Returned more than 1 row for id ", id);
+        res.sendStatus(500); // data integrity
+      } else if(rows.length == 0){
+        res.send(null); //don't send anything back
+        // OR
+        res.status(404).send('User not Found');
+      } else {
+        // success -> We got one row.
+        res.json(rows[0]) // if return just res.json(rows), it returns an array with one object
+                      // if I want to return just the array, I need to reference the index
+      }
+    }
+
+  })
+// OLDER SYNTAX - WILL NO LONGER USE:
+// WHAT GOES IN THE BRACKETS
 //   sql = mysql.format(sql, [])
 
 //   pool.query(sql, (err, rows) => {
@@ -59,161 +93,98 @@ const getUserById = (req, res) => {
 //     return res.json(rows);
 //   })
 // }
-
-const createUser = (req, res) => {
-
-  //FIRST QUERY -> INSERT INTO USERS FIRST AND LAST NAME 
-  let first = req.body.first_name
-  let last = req.body.last_name
-
-  let params = [first, last]
-  let sql = "INSERT INTO users (first_name, last_name) VALUES (?, ?)"
-
-  let results
-
-  try {
-    results = await db.querySync(sql, params)
-  } catch(err) {
-    console.log("createUser query failed ", err)
-    res.sendStatus(500)
-    return // if query didn't work, stop
-  }
-
-  let id = results.insertId
-
-
-  //SECOND QUERY -> INSERT INTO usersAddress: user ID, address, city, county, state, zip
-
-  let address = req.body.address
-  let city = req.body.city
-  let county =  req.body.county
-  let state = req.body.state
-  let zip = req.body.zip
-
-  params = [id, address, city, county, state, zip]
-  sql = "INSERT INTO usersAddress (user_id, address, city, county, state, zip) VALUES (?, ?, ?, ?, ?, ?)"
-
-  try {
-    results = await db.querySync(sql, params)
-  } catch(err) {
-    console.log("createUser address query failed ", err)
-    res.sendStatus(500)
-    return
-  }
-
-
-  //THIRD QUERY -> INSERT INTO usersContact: user ID, phone1, phone2, email
-  let phone1 = req.body.phone1
-  let phone2 = req.body.phone2
-  let email = req.body.email
-  
-  params = [id, phone1, phone2, email]
-  sql = "INSERT INTO usersContact (user_id, phone1, phone2, email) VALUES (?, ?, ?, ?)"
-
-  try {
-    results = await db.querySync(sql, params)
-  } catch(err) {
-    console.log("createUser contact query failed ", err)
-    res.sendStatus(500)
-    return
-  }
-
 }
 
+const createUser = (req, res) => {
+  // INSERT INTO USERS FIRST AND LAST NAME - when the id is created that we need for the user_id columns in the other two tables
+  // So, the first insert MUST complete before we can execute the other 2 queries.
+  // INSERT INTO usersContact user_id, phone1, phone2, email
+  // INSERT INTO usersAddress user_id, address, city, county, state, zip
 
+  // async run something until it finishes whenever, and goes on to run other stuff 
+  // while that first thing may or may not be finished.
+  // Sometimes you need code to run in a certain order (like fetch & promises)
+  // This is called BLOCKING CODE sometimes.
 
+  // Here's the kicker: mySQL doesn't have built-in methods to create blocking code out of the box.
 
+  // One way we handle this is to create nested callbacks that execute each query one at a time
+  // This can get complicated. We call this scenario Callback Hell.
 
+  // CALLBACK HELL VERSION
 
+  // FIRST QUERY
+  let first = req.body.first_name;
+  let last = req.body.last_name;
+  let params = [first, last];
 
+  //I could also do this, but sometimes it gets long
+  // let params = [req.body.first_name, req.body.last_name]
+  let sql = "insert into users (first_name, last_name) values (?, ?)";
 
-
-/**
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- */
-const updateUserById = (req, res) => {
-  // UPDATE USERS AND SET FIRST AND LAST NAME WHERE ID = <REQ PARAMS ID>
-  let id = req.params.id
-  let first = req.body.first_name
-  let last = req.body.last_name
-  let params = [first, last, id]
-
-  let sql = "UPDATE users u "
-  sql += "SET first_name = ?, last_name = ? "
-  sql += "WHERE id = ?"
-
-  if(!id){
-    res.sendStatus(400)
-    return
-  }
-
-  // WHAT GOES IN THE BRACKETS
-  db.query(sql, params, (err, rows) => {
-    if (err){
-      console.log("updateUserById query failed", err)
-      res.sendStatus(400)
+  db.query(sql, params, (err,rows) => {
+    if(err){
+      console.log("createUser query failed ", err);
+      res.sendStatus(500);
     } else {
-      res.json(rows);
+      // If we got here, the first query executed
+      // postman check
+      
+      // SECOND QUERY
+      // I need the ID of the user we just inserted. For this example, I'm might have to use MAX() to get the id
+      let getId = rows.insertId;
+
+      let address = req.body.address;
+      let city = req.body.city;
+      let county = req.body.county;
+      let state = req.body.state;
+      let zip = req.body.zip;
+
+      params = [getId, address, city, county, state, zip]
+
+      sql = "insert into usersAddress (user_id, address, city, county, state, zip) ";
+      sql += "values (?,?,?,?,?,?)";
+
+      db.query(sql, params, (err,rows) => {
+        if(err){
+          console.log("insert into usersAddress query failed ", err)
+        } else {
+          // second query worked
+          // postman check
+          // res.json(rows);
+
+          //THIRD QUERY
+        }
+      })
+      
+
     }
   })
 }
 
+const updateUserById = (req, res) => {
+  // UPDATE USERS AND SET FIRST AND LAST NAME WHERE ID = <REQ PARAMS ID>
+  let sql = ""
+  // WHAT GOES IN THE BRACKETS
+  sql = mysql.format(sql, [])
 
-
-//   //OLDER SYNTAX:
-//   sql = mysql.format(sql, [])
-
-//   pool.query(sql, (err, results) => {
-//     if (err) return handleSQLError(res, err)
-//     return res.status(204).json();
-//   })
-// }
-
-
-
+  pool.query(sql, (err, results) => {
+    if (err) return handleSQLError(res, err)
+    return res.status(204).json();
+  })
+}
 
 const deleteUserByFirstName = (req, res) => {
   // DELETE FROM USERS WHERE FIRST NAME = <REQ PARAMS FIRST_NAME>
-  let first = req.params.first_name
-  let params = [first]
-
-  let sql = "DELETE FROM users WHERE first_name = ?"
-
-
-
-
+  let sql = ""
   // WHAT GOES IN THE BRACKETS
-  if(!first){
-    res.sendStatus(400)
-    return
-  }
+  sql = mysql.format(sql, [])
 
-  db.query(sql, params, (err, rows) => {
-    if (err){
-      console.log("deleteUserByFirstName query failed", err)
-      res.sendStatus(400)
-    } else {
-      res.json({ message: `Deleted ${rows.affectedRows} user(s)`});
-    }
+  pool.query(sql, (err, results) => {
+    if (err) return handleSQLError(res, err)
+    return res.json({ message: `Deleted ${results.affectedRows} user(s)` });
   })
 }
-
-//   //OLDER SYNTAX:
-//   sql = mysql.format(sql, [])
-
-//   pool.query(sql, (err, results) => {
-//     if (err) return handleSQLError(res, err)
-//     return res.json({ message: `Deleted ${results.affectedRows} user(s)` });
-//   })
-// }
 
 module.exports = {
   getAllUsers,
@@ -222,3 +193,6 @@ module.exports = {
   updateUserById,
   deleteUserByFirstName
 }
+
+
+
---
+++ sql/sample-object.js
@@ -0,0 +1,11 @@
+{
+    "first_name": "Ashley",
+    "last_name": "Brooks",
+    "address": "123 Main Street",
+    "city": "Austin",
+    "state": "TX",
+    "zip": "78701",
+    "phone1": "512-123-4567",
+    "phone2": "504-345-6789",
+    "email": "ashley@here.com"
+}
